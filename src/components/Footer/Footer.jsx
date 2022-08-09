1cd6552a7a1022fb3c04d6082a9aea0269c3e4c7
satya <satyaranjanrayrazzor@gmail.com>
2022-08-10 01:33:24 +0530

    "v1"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,62 @@
 import React from 'react'
-import {Container, Row,Col} from 'reactstrap'
+import {Container, Row,Col, ListGroup, ListGroupItem} from 'reactstrap'
+import {Link} from 'react-router-dom'
+
+
+const myAccount=[
+  {
+    display:'Stanley Profile',
+    url:'/seller-profile'
+  },
+  {
+    display:'Create Item',
+    url:'/create'
+  },
+  {
+    display:'Collections',
+    url:'/market'
+  },
+  {
+    display:'Edit Profile',
+    url:'/edit-profile'
+  }
+]
+const resources=[
+  {
+    display:'Help Center',
+    url:'#'
+  },
+  {
+    display:'Documentation',
+    url:'#'
+  },
+  {
+    display:'Community',
+    url:'/market'
+  },
+  {
+    display:'Market Capitalization',
+    url:'#'
+  }
+]
+const company=[
+  {
+    display:'About',
+    url:'#'
+  },
+  {
+    display:'Career',
+    url:'#'
+  },
+  {
+    display:'RoadMap',
+    url:'/market'
+  },
+  {
+    display:'Contact us',
+    url:'#'
+  }
+]
 
 const Footer = () => {
   return <footer className='footer'>
@@ -15,12 +72,47 @@ const Footer = () => {
             </div>
             </Col>
 
-            <Col lg='2' md='3' sm='6'></Col>
-            <Col lg='2' md='3' sm='6'></Col>
-            <Col lg='2' md='3' sm='6'></Col>
+            <Col lg='2' md='3' sm='6'>
+              <h5>My Account!</h5>
+              <ListGroup className='list__group'>
+                {myAccount.map((i,index)=>(
+                  <ListGroupItem key={index} className='list__item'>
+                    <Link to={i.url}>{i.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </Col>
+            <Col lg='2' md='3' sm='6'>
+            <h5>Resources!</h5>
+              <ListGroup className='list__group'>
+                {resources.map((i,index)=>(
+                  <ListGroupItem key={index} className='list__item'>
+                    <Link to={i.url}>{i.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </Col>
+            <Col lg='2' md='3' sm='6'>
+            <h5>Company!</h5>
+              <ListGroup className='list__group'>
+                {company.map((i,index)=>(
+                  <ListGroupItem key={index} className='list__item'>
+                    <Link to={i.url}>{i.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </Col>
 
             <Col lg='3' md='6' sm='6'>
-
+                  <h5>Important Updates!</h5>
+                  <input type="text" className="updates" placeholder="Email"/>
+                  <div className='social-links'>
+                    {/* <span ><Link to='#'><i class="ri-github-fill"></i></Link></span>
+                    <span ><Link to='#'><i class="ri-github-fill"></i></Link></span>
+                    <span ><Link to='#'><i class="ri-github-fill"></i></Link></span>
+                    <span ><Link to='#'><i class="ri-github-fill"></i></Link></span>
+                    <span ><Link to='#'><i class="ri-github-fill"></i></Link></span> */}
+                  </div>
             </Col>
           </Row>
         </Container>
