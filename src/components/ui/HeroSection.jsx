48873a1749eb673390a3ee1ea950e2f770f6df12
satya <satyaranjanrayrazzor@gmail.com>
2022-08-10 16:49:21 +0530

    "button Logo added"

--- src/components/ui/HeroSection.jsx
+++ src/components/ui/HeroSection.jsx
@@ -12,8 +12,8 @@ const HeroSection = () => {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quo maiores animi temporibus id, nostrum sed nobis labore illo reprehenderit eum corporis nesciunt magnam. Cum, officiis? Harum praesentium iure amet.</p>
                 
                 <div className="hero__btns d-flex align-items-center gap-4">
-                <button className="btn1"><Link to='/market'>Explore More</Link></button>
-                <button className="btn1"><Link to='/create'>Self Craft</Link></button>
+                <button className="btn1"><i class="ri-store-2-line"></i> <Link to='/market'>Explore More</Link></button>
+                <button className="btn1"><i class="ri-edit-2-fill"></i> <Link to='/create'>Self Craft</Link></button>
                 </div>
             </Col>
         </Row>
